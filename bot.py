dfeca6ddcff8b95b8dd90a0b7686fddb0ca47b3d
paci_369 <antoniopacillo12@gmail.com>
2020-05-20 15:02:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ telegrambot = telepot.Bot(token)
 chat_id = canale
 
 while True:
-    daily_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
-    request = requests.get(daily_url)
-    req_json = request.json()
-    daily_pic = req_json['url']
-    daily_caption = req_json['date']+" ~ "+req_json['title']+" ~ Copyright: "+req_json['copyright']+"\n"+req_json['explanation']
     ora = datetime.datetime.now().time()
-    invio = ora.replace(hour = 7, minute = 0, microsecond = 0)
+    invio = ora.replace(hour = 7, minute = 0, second = 0)
     if ora == invio:
+        daily_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+        request = requests.get(daily_url)
+        req_json = request.json()
+        daily_pic = req_json['url']
+        daily_caption = req_json['date']+" ~ "+req_json['title']+" ~ Copyright: "+req_json['copyright']+"\n"+req_json['explanation']
         telegrambot.sendPhoto(chat_id, daily_pic, daily_caption)
     time.sleep(2)
