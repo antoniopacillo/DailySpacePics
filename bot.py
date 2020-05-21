0990aff125a47fc3b1d749dd6fc9c800a862b97a
paci_369 <antoniopacillo12@gmail.com>
2020-05-21 09:50:54 +0200

    Risoluzione di alcuni problemi
    1 modificato il tempo di attesa a 1 secondo per evitare di "saltare" l'ora attuale da controllare
    2 risolto il problema che si presenta quando la descrizione supera la lunghezza di 1024 caratteri, così nel caso si trovasse in quella situazione il bot invia 2 messaggi (uno con la foto e le informazioni base e uno con la descrizione)
    3 ottimizzata la connessione all'API tenendo l'inizializzazione fuori da ciclo principale

--- bot.py
+++ bot.py
@@ -3,15 +3,21 @@ from credenziali import *
 
 telegrambot = telepot.Bot(token)
 chat_id = canale
+daily_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+request = requests.get(daily_url)
+req_json = request.json()
 
 while True:
     ora = datetime.datetime.now().time()
-    invio = ora.replace(hour = 9, minute = 0, second = 0)
+    invio = ora.replace(hour = 9, minute = 43, second = 0)
     if ora == invio:
-        daily_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
-        request = requests.get(daily_url)
-        req_json = request.json()
         daily_pic = req_json['url']
-        daily_caption = req_json['date']+" ~ "+req_json['title']+" ~ Copyright: "+req_json['copyright']+"\n"+req_json['explanation']
-        telegrambot.sendPhoto(chat_id, daily_pic, daily_caption)
-    time.sleep(2)
+        if len(req_json['date']+" ~ "+req_json['title']+" ~ Copyright: "+req_json['copyright']+"\n"+req_json['explanation']) > 1024:
+            daily_caption = req_json['date']+" ~ "+req_json['title']+" ~ Copyright: "+req_json['copyright']
+            description = req_json['explanation']
+            telegrambot.sendPhoto(chat_id, daily_pic, daily_caption)
+            telegrambot.sendMessage(chat_id, description)
+        else:
+            daily_caption = req_json['date']+" ~ "+req_json['title']+" ~ Copyright: "+req_json['copyright']+"\n"+req_json['explanation']
+            telegrambot.sendPhoto(chat_id, daily_pic, daily_caption)
+    time.sleep(1)
